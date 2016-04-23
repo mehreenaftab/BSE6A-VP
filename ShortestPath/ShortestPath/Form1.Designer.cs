f32be886c8ffd413e848aa22e78afab8cb0afe60
mehreenaftab <mehreen-148@hotmail.com>
2016-04-23 09:18:48 -0700

    project work

---
+++ ShortestPath/ShortestPath/Form1.Designer.cs
@@ -0,0 +1,102 @@
+﻿namespace ShortestPath
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.MistyRose;
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.textBox1);
+            this.panel1.Location = new System.Drawing.Point(2, 1);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(191, 385);
+            this.panel1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(45, 174);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(100, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Generate Nodes";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(42, 100);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(95, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Enter No of Nodes";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(45, 127);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            this.textBox1.Leave += new System.EventHandler(this.Textbox_Leave);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LavenderBlush;
+            this.ClientSize = new System.Drawing.Size(854, 386);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ ShortestPath/ShortestPath/Form1.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ShortestPath
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        Graphics drawcircle;
+        
+        GraphicsPath p = new GraphicsPath();
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string lab_text = "Enter No of Nodes";
+            label1.Text = lab_text;                 //Assigning  Text to the label using its Text property through code
+            string btn_text = "Generate Nodes";
+            button1.Text = btn_text;               //Assigning  Text to the button using its Text proerty through code
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            //string text = "Enter Number of Nodes";
+            //label1.Text = text;
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+          //  if(textBox1.Text ==)
+        }
+
+        private void Textbox_Leave(object sender, EventArgs e)
+        {
+            int input = Convert.ToInt32(textBox1.Text);
+            if (input > 10)
+            {
+                MessageBox.Show("You can not enter more than 10 nodes");
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            int input = Convert.ToInt32(textBox1.Text);
+            if (input > 10)
+            {
+                MessageBox.Show("You can not enter more than 10 nodes");
+            }
+            else
+            {
+                for(int i = 1 ; i <= input ; i++)
+                {
+                    Button butn = new Button();
+                    butn.width = 50;
+                    butn.BackColor                 
+                
+                }
+            
+            }
+
+            
+            
+            //Brush brush = new Brush();
+            //drawcircle.FillEllipse(brush,10,20,30,40);
+
+            //Graphics graphics = e.Graphics;
+           // Graphics drawcircle = new Graphics();
+            Pen myPen = new Pen(Color.Black);
+            // Draw the button in the form of a circle
+            //for()
+            button2.Width = 45;
+            button2.Height = 45;
+            GraphicsPath p = new GraphicsPath();
+            p.AddEllipse(4, 4, button2.Width - 8, button2.Height - 8);
+            button1.Region = new Region(p);
+           // drawcircle.DrawEllipse(myPen, 0, 0, 100, 100);
+           // myPen.Dispose();
+         
+        }
+
+    }
+}
